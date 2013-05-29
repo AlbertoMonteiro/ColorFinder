46f5e12174d5ed334add6cf8e3401508a128f63a
Alberto Monteiro <alberto.monteiro@live.com>
2013-05-29 01:43:03 -0300

    Working

--- src/ColorFinder/Form1.Designer.cs
+++ src/ColorFinder/Form1.Designer.cs
@@ -30,23 +30,26 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
             this.textBox3 = new System.Windows.Forms.TextBox();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.textBox4 = new System.Windows.Forms.TextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.panel3 = new System.Windows.Forms.Panel();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(285, 117);
+            this.label1.Location = new System.Drawing.Point(100, 80);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(60, 13);
             this.label1.TabIndex = 1;
@@ -58,26 +61,18 @@
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.panel1.Location = new System.Drawing.Point(12, 12);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(260, 260);
+            this.panel1.Size = new System.Drawing.Size(85, 119);
             this.panel1.TabIndex = 3;
-            // 
-            // panel2
-            // 
-            this.panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel2.Location = new System.Drawing.Point(288, 12);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(260, 30);
-            this.panel2.TabIndex = 4;
             // 
             // tabControl1
             // 
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Location = new System.Drawing.Point(288, 49);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(103, 12);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(260, 59);
+            this.tabControl1.Size = new System.Drawing.Size(353, 59);
             this.tabControl1.TabIndex = 5;
             // 
             // tabPage1
@@ -86,21 +81,10 @@
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(252, 33);
+            this.tabPage1.Size = new System.Drawing.Size(317, 33);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "ARGB";
             this.tabPage1.UseVisualStyleBackColor = true;
-            // 
-            // tabPage2
-            // 
-            this.tabPage2.Controls.Add(this.textBox3);
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(252, 33);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "Hexadecimal";
-            this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // textBox2
             // 
@@ -108,8 +92,19 @@
             this.textBox2.Location = new System.Drawing.Point(3, 3);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(246, 27);
+            this.textBox2.Size = new System.Drawing.Size(311, 27);
             this.textBox2.TabIndex = 3;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.textBox3);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(317, 33);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Hexadecimal";
+            this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // textBox3
             // 
@@ -117,33 +112,66 @@
             this.textBox3.Location = new System.Drawing.Point(3, 3);
             this.textBox3.Multiline = true;
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(246, 27);
+            this.textBox3.Size = new System.Drawing.Size(311, 27);
             this.textBox3.TabIndex = 4;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.textBox4);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(345, 33);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Color diff";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox4.Location = new System.Drawing.Point(3, 3);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(339, 27);
+            this.textBox4.TabIndex = 0;
+            this.textBox4.Text = "original_color: 95B8E7, red:-76.42, green:-80.28, blue:-91.67";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(351, 114);
+            this.textBox1.Location = new System.Drawing.Point(166, 77);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(197, 20);
+            this.textBox1.Size = new System.Drawing.Size(79, 20);
             this.textBox1.TabIndex = 2;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TextBox1KeyDown);
             // 
             // panel3
             // 
             this.panel3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel3.Location = new System.Drawing.Point(288, 140);
+            this.panel3.Location = new System.Drawing.Point(103, 103);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(260, 30);
+            this.panel3.Size = new System.Drawing.Size(353, 30);
             this.panel3.TabIndex = 5;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(349, 79);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(107, 17);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "Clipboard monitor";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.CheckBox1CheckedChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(556, 282);
+            this.ClientSize = new System.Drawing.Size(468, 143);
+            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.tabControl1);
-            this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
@@ -154,6 +182,8 @@
             this.tabPage1.PerformLayout();
             this.tabPage2.ResumeLayout(false);
             this.tabPage2.PerformLayout();
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -163,7 +193,6 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.TabControl tabControl1;
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.TextBox textBox2;
@@ -171,6 +200,9 @@
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
 
--- src/ColorFinder/Form1.cs
+++ src/ColorFinder/Form1.cs
@@ -1,101 +1,152 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Drawing.Imaging;
-using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using Microsoft.VisualBasic.PowerPacks;
+using ColorFinder.UserActivityMonitor;
 
 namespace ColorFinder
 {
     public partial class Form1 : Form
     {
-        private Point lastPosition;
-        private bool firstTime;
-        private const int WM_MOUSEMOVE = 0x0200;
+        private const int WM_DRAWCLIPBOARD = 0x308;
+        private const int WM_CHANGECBCHAIN = 0x030D;
 
+        IntPtr nextClipboardViewer;
 
-        [DllImport("user32.dll")]
-        static extern bool GetCursorPos(ref Point lpPoint);
+        [DllImport("User32.dll")]
+        protected static extern int SetClipboardViewer(int hWndNewViewer);
 
+        [DllImport("User32.dll", CharSet = CharSet.Auto)]
+        public static extern bool ChangeClipboardChain(IntPtr hWndRemove, IntPtr hWndNewNext);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern int SendMessage(IntPtr hwnd, int wMsg, IntPtr wParam, IntPtr lParam);
+        
         [DllImport("gdi32.dll", CharSet = CharSet.Auto, SetLastError = true, ExactSpelling = true)]
         public static extern int BitBlt(IntPtr hDC, int x, int y, int nWidth, int nHeight, IntPtr hSrcDC, int xSrc, int ySrc, int dwRop);
 
         public Form1()
         {
-            firstTime = true;
             InitializeComponent();
 
-            Timer timer = new Timer();
-            timer.Tick += MouseMoveTimer_Tick;
-            timer.Interval = 100;
-            timer.Start();
+            nextClipboardViewer = (IntPtr)SetClipboardViewer((int)Handle);
+            Load += (sender, args) =>
+            {
+                HookManager.MouseMove += HookManagerMouseMove;
+                /*HookManager.KeyDown += HookManagerKeyDown;*/
+            };
         }
 
-        private void MouseMoveTimer_Tick(object sender, EventArgs e)
+        private void HookManagerMouseMove(object sender, MouseEventArgs e)
         {
-            Point cursor = new Point();
-            GetCursorPos(ref cursor);
-            if (firstTime)
-            {
-                lastPosition = cursor;
-                Text = GetColorAt(cursor).ToString();
-                firstTime = false;
-                System.Diagnostics.Debug.WriteLine("firstTime");
-            }
-
-            if (lastPosition.X != cursor.X && lastPosition.Y != cursor.Y)
-            {
-                System.Diagnostics.Debug.WriteLine("{0} - {1}", lastPosition, cursor);
-                lastPosition = cursor;
-                Text = GetColorAt(cursor).ToString();
-                System.Diagnostics.Debug.WriteLine("change");
-            }
+            GetColorAt(e.Location);
         }
 
-
-        public Color GetColorAt(Point location)
+        public void GetColorAt(Point point)
         {
-            Color color = Color.White;
-            Bitmap screenPixel = new Bitmap(17, 17, PixelFormat.Format32bppArgb);
+            var screenPixel = new Bitmap(1, 1, PixelFormat.Format32bppArgb);
             using (var gdest = Graphics.FromImage(screenPixel))
             {
                 using (var gsrc = Graphics.FromHwnd(IntPtr.Zero))
                 {
                     var hSrcDC = gsrc.GetHdc();
                     var hDC = gdest.GetHdc();
-                    int retval = BitBlt(hDC, 0, 0, 17, 17, hSrcDC, location.X - 8, location.Y - 8, (int)CopyPixelOperation.SourceCopy);
+                    int retval = BitBlt(hDC, 0, 0, 1, 1, hSrcDC, point.X, point.Y, (int)CopyPixelOperation.SourceCopy);
                     gdest.ReleaseHdc();
                     gsrc.ReleaseHdc();
                 }
             }
 
-            Bitmap background = new Bitmap(272, 272, PixelFormat.Format32bppArgb);
+            panel1.BackColor = screenPixel.GetPixel(0, 0);
+            textBox2.Text = string.Format("{0},{1},{2},{3}", (int)panel1.BackColor.A, (int)panel1.BackColor.R, (int)panel1.BackColor.G, (int)panel1.BackColor.B);
+            textBox3.Text = string.Format("#{0:X4}", panel1.BackColor.ToArgb());
+            textBox4.Text = CheckColorDiff(panel1.BackColor);
+        }
 
-            for (int i = 0; i < screenPixel.Width; i++)
+        private string CheckColorDiff(Color colorB)
+        {
+            double basis, diff, percentDiff = 0;
+
+            Color colorA = panel3.BackColor;
+
+            Func<int, int, string> colorDiff = (channelA, channelB) =>
             {
-                for (int i2 = 0; i2 < 15; i2++)
+                if (channelA > channelB)
                 {
-                    for (int j = 0; j < screenPixel.Height; j++)
-                    {
-                        var pixel = screenPixel.GetPixel(i, j);
-                        for (int k = 0; k < 15; k++)
-                        {
-                            background.SetPixel(i, j, Color.FromArgb(pixel.ToArgb()));
-                        }
-                    }
+                    basis = channelA;
+                    diff = channelA - channelB;
+                    percentDiff = -(diff/(basis/100));
+                }
+
+                if (channelA < channelB)
+                {
+                    basis = 255 - channelA;
+                    diff = channelB - channelA;
+                    percentDiff = -(diff/(basis/100));
+                }
+                return percentDiff.ToString("0.00");
+            };
+
+
+            return string.Format("original_color: {0:X4}, red:{1}, green:{2}, blue:{3}", colorA.ToArgb(), colorDiff(colorA.R, colorB.R), colorDiff(colorA.G, colorB.G), colorDiff(colorA.B, colorB.B));
+        }
+
+        private void TextBox1KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                var replace = textBox1.Text.Replace("#", "");
+                if (replace.Length == 2)
+                {
+                    replace = "FF" + replace + replace + replace;
+                    textBox1.Text = string.Format("#{0}", replace);
+                    var a = Convert.ToInt32(replace, 16);
+                    panel3.BackColor = Color.FromArgb(a);
+                }
+                else if (replace.Length == 3)
+                {
+                    replace = "FF" + replace + replace;
+                    textBox1.Text = string.Format("#{0}", replace);
+                    var a = Convert.ToInt32(replace, 16);
+                    panel3.BackColor = Color.FromArgb(a);
+                }
+                else if (replace.Length == 6)
+                {
+                    replace = "FF" + replace;
+                    textBox1.Text = string.Format("#{0}", replace);
+                    var a = Convert.ToInt32(replace, 16);
+                    panel3.BackColor = Color.FromArgb(a);
+                }
+                else if (replace.Length == 8)
+                {
+                    var a = Convert.ToInt32(replace, 16);
+                    panel3.BackColor = Color.FromArgb(a);
                 }
             }
-
-            panel1.BackgroundImage = screenPixel;
-            System.Diagnostics.Debug.WriteLine(screenPixel.GetPixel(0, 0).ToString());
-            //color = screenPixel.GetPixel(0, 0);
-            return color;
         }
+
+        private void CheckBox1CheckedChanged(object sender, EventArgs e)
+        {
+        }
+
+/*
+        private void HookManagerKeyDown(object sender, KeyEventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                if (e.Control || e.KeyCode == Keys.Control || e.KeyCode == Keys.ControlKey)
+                {
+                    if (e.KeyCode == Keys.C)
+                    {
+                        var checkColorDiff = CheckColorDiff(panel1.BackColor);
+                        Clipboard.SetText(checkColorDiff);
+                        Debug.WriteLine(checkColorDiff);
+                    } 
+                }
+            }
+        }
+*/
     }
 }
